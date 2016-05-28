9ffebc60fd193ef0afd3056b3860bf85df7ac90d
Anselmo Pfeifer <anselmo.pfeifer@tecsinapse.com.br>
2016-05-27 23:14:49 -0400

    teste

--- Projeto01/src/sinaleira.ino
+++ Projeto01/src/sinaleira.ino
@@ -1,25 +1,39 @@
 #include <Arduino.h>
 
-const int VERDE = 4;
-const int VERMELHO = 5;
-const int  BUTTON = 6;
+const int LED = 5;
+const int STEP = 10;
+const int PWM_MIN = 0;
+const int PWM_MAX = 255;
+int value = PWM_MIN;
 
-int STATE = 0;
-
-void setup(){
-  pinMode(VERDE, OUTPUT);
-  pinMode(VERMELHO, OUTPUT);
-  pinMode(BUTTON, INPUT);
-
-  digitalWrite(VERDE, LOW);
-  digitalWrite(VERMELHO, LOW);
+void setup() {
+  pinMode(LED, OUTPUT);
+  analogWrite(LED, value);
+  Serial.begin(9600);
 }
 
-void loop(){
-  STATE = digitalRead(BUTTON);
-  if (STATE == HIGH) {
-      digitalWrite(VERMELHO, HIGH);
-  }else{
-    digitalWrite(VERMELHO, LOW);
+void loop() {
+  char data = 0;
+  if (Serial.available() > 0)
+  {
+    data = Serial.read();
+    switch (data)
+    {
+      case '+' :
+        value += STEP;
+        if (value > PWM_MAX) value = PWM_MAX;
+        break;
+      case '-':
+        value -= STEP;
+        if (value < PWM_MIN) value = PWM_MIN;
+        break;
+      case 'z':
+        value = PWM_MIN;
+        break;
+      case 'm':
+        value = PWM_MAX;
+        break;
+    }
+    analogWrite(LED, value);
   }
 }
