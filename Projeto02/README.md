062fd94c0c498a4adf6e58416f08d9a80c1eb919
Anselmo Pfeifer <anselmo.pfeifer@tecsinapse.com.br>
2016-05-28 20:44:33 -0400

    Add o projeto02 que recebe um sinal do botao para piscar um led.

---
+++ Projeto02/README.md
@@ -0,0 +1,5 @@
+# entradaSinal
+  O arduino recebe um sinal positivo a partir
+  de um acionamento do botao e pisca o led vermelho.
+
+  OBS: o led verde continua acesso.
---
+++ Projeto02/src/entradaSinal.ino
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+const int LED_VERMELHO = 9;
+const int LED_VERDE = 10;
+const int BUTTON = 2;
+
+int primeiroTempo = 500;
+int segundoTempo = 5000;
+
+void setup() {
+  pinMode(LED_VERMELHO, OUTPUT);
+  pinMode(LED_VERDE, OUTPUT);
+  pinMode(BUTTON, INPUT);
+
+  digitalWrite(LED_VERMELHO, LOW);
+  digitalWrite(LED_VERDE, LOW);
+}
+
+void loop() {
+  if (digitalRead(BUTTON) == HIGH) {
+    digitalWrite(LED_VERMELHO, HIGH);
+    delay(primeiroTempo);
+    digitalWrite(LED_VERMELHO, LOW);
+    delay(primeiroTempo);
+  }else if(digitalRead(BUTTON) == LOW){
+    digitalWrite(LED_VERMELHO, LOW);
+    digitalWrite(LED_VERDE, HIGH);
+  }
+}
